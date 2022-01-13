fbf0910cabcfae379bc4e444bc9dbdc1b5acb3ea
Angela Billingsley <abill99@yahoo.com>
2022-01-13 00:18:13 -0500

    Update index.html and scripts.js files for Calculator to hide and show calculator

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,26 @@ function add(number1, number2){
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("enter a number:"));
-const number2 = parseInt(prompt("enter another number:"));
-const result = add(number1, number2);
-alert(result);
+
+$(document).ready(function() {
+  $("#showButton").click(function() {
+    $(".showing").toggle();
+    $(".hiding").toggle();
+   // $(".showing").toggle();
+    //$(".hiding").toggle();
+  });
+
+ 
+
+  $( "#addButton" ).click(function() {
+    alert( "Welcome to the Calculator. Click to continue" );
+    const n1 = parseInt(prompt("enter a number:"));
+    const n2 = parseInt(prompt("enter another number:"));
+    const result = add(n1, n2);
+    alert(result);
+  });
+
+  $(".hiding").hide();
+
+
+});
